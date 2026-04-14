9deb5fe39bfe3320a8bbf70230de720f4216a361
BryceD-4 <144172482+BryceD-4@users.noreply.github.com>
2026-04-13 17:32:09 -0700

    Refactor Enums code
    All enum files were updated to have same opening description.

--- src/Domain/Enums/CropState.cs
+++ src/Domain/Enums/CropState.cs
@@ -1,3 +1,11 @@
+/*
+PROJECT: Automated Greenhouse
+AUTHOR: Bryce Dixon
+DESCRIPTION:
+- Used to create an enum for CropState to avoid typos or confusion
+
+*/
+
 namespace Greenhouse.Domain.Enums;
 
 public enum CropState
--- src/Domain/Enums/RobotState.cs
+++ src/Domain/Enums/RobotState.cs
@@ -1,3 +1,10 @@
+/*
+PROJECT: Automated Greenhouse
+AUTHOR: Bryce Dixon
+DESCRIPTION:
+- Used to create an enum for RobotState to avoid typos or confusion
+
+*/
 namespace Greenhouse.Domain.Enums;
 
 public enum RobotState
